817c2be11272d22e71bc5fa6ad3b3b074f638910
Nao Suzuki <39011805+yonkzs@users.noreply.github.com>
2022-02-01 14:49:39 +0700

    Generate HTML throught Pug

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,20 @@ module.exports = {
           // },
         ],
       },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
@@ -45,7 +59,12 @@ module.exports = {
       filename: './stylesheets/main.css',
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/index.html',
+      template: './src/templates/index.pug',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/access.pug',
+      filename: 'access.html',
     }),
     new CleanWebpackPlugin(),
   ],
